e8e1f33eec305fd67946c3a60dc0d7fe60e80bd4
T1092419 Gabriel Ribeiro De Sousa <t1092419@interno.bb.com.br>
2024-12-19 11:34:32 -0300

    DB2_PORT como string

--- src/core/config.py
+++ src/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB2_HOST: str
     DB2_DB: str
-    DB2_PORT: int
+    DB2_PORT: str
     DB2_USER: str
     DB2_PASS: str
 
