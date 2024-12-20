1ddd61bf9838a02b225af78d67c0e1121054a766
T1092419 Gabriel Ribeiro De Sousa <t1092419@interno.bb.com.br>
2024-12-20 14:53:04 -0300

    prefixo da aplicação mudado para intc_cnxo

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from src.core.auth import AuthMiddleware
 from src.routers import api
 
+BASE_PREFIX = '/intc_cnxo'
+
 app = FastAPI()  # Inicialização do app
 
+main_router = APIRouter(prefix=BASE_PREFIX)
+main_router.include_router(api.router)
+app.include_router(main_router)
+
+app.mount(f'{BASE_PREFIX}', StaticFiles(directory='static', html=True))
+
 app.add_middleware(AuthMiddleware)
-
-app.include_router(api.router)
-
-app.mount('/', StaticFiles(directory='static', html=True))
