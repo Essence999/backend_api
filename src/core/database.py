5d71ad78e28dfb0de9e10e6e300e4efd305c2188
T1092419 Gabriel Ribeiro De Sousa <t1092419@interno.bb.com.br>
2024-12-26 11:33:12 -0300

    pooling de conexões para melhorar a performance

--- src/core/database.py
+++ src/core/database.py
@@ -2,14 +2,17 @@ from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
-from sqlalchemy.pool import NullPool
 
 from src.core.config import Settings
 
 sett = Settings()
 uri = f'{sett.DB2_USER}:{sett.DB2_PASS}@{sett.DB2_HOST}:{sett.DB2_PORT}/{sett.DB2_DB}'
 
-engine = create_engine(f'ibm_db_sa://{uri}', poolclass=NullPool)
+engine = create_engine(f'ibm_db_sa://{uri}',
+                       pool_size=1,
+                       max_overflow=2,
+                       pool_timeout=60,
+                       pool_recycle=3600)
 
 
 def get_db() -> Generator[Session, None, None]:
